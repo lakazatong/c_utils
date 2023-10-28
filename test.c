2544527a0a13fd847f53f05baa94cce3c6d41f3b
lakazatong <Bo_wood@outlook.fr>
2023-10-28 03:46:06 +0200

    revamp structure and better makefile
    just like beatmap_parser

--- test.c
+++ test.c
@@ -1,10 +1,32 @@
-/*
-	Uncomment to run isolated tests on c_utils only.
-*/
-/*
-#include "utils/all.h"
+#include <time.h>
 
-int WinMain() {
+#include "src/all.h"
+
+#define MAIN_BODY_LINUX 
+
+#define MAIN_BODY_WIN 
+
+#ifndef _WIN32
+
+int main(int argc, char* argv[]){
+	clock_t begin = clock();
+	MAIN_BODY_LINUX
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("time of execution: %lf\n", time_spent);
 	return 0;
 }
-*/
+
+#else
+
+#include <windows.h>
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+	clock_t begin = clock();
+	MAIN_BODY_WIN
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("time of execution: %lf\n", time_spent);
+	return 0;
+}
+
+#endif
